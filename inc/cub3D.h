8b3c4c43bab856d5a6d3cae81ac5b977032df60d
Neves-97 <daminaneves@gmail.com>
2024-03-25 14:55:31 +0000

    yo

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: neves <neves@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 14:53:49 by neves             #+#    #+#             */
+/*   Updated: 2024/03/25 14:53:50 by neves            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 # define CUB3D_H
 
@@ -13,45 +25,86 @@
 # include "structs.h"
 # include "defines.h"
 
-t_img	new_image(int w, int h, t_win window);
-void	put_pixel(t_img img, int x, int y, int color);
-int		key_hook(int keycode);
-int		free_all(int exit_status);
-t_var	*vars(void);
-t_win	init_window(int w, int h, char *str);
-void	draw_floor_ceiling(int width, int height, t_pos pos);
-void	init_rays(void);
+// check_horizontal.c
+
 void	check_horizontal(double ra);
-void	wall_color(void);
+
+// check_map.c
+
+int		check_map(void);
+
+// check_vertical.c
+
 void	check_vertical(double ra);
-void	inits(char *file_path);
-void	init_player(void);
-t_pos	init_player_pos(void);
-void	draw_all(void);
+
+// colors.c
+
+void	color_limits(int *red, int *green, int *blue);
+int		rgb_to_int(int red, int green, int blue);
+void	wall_color(void);
+
+// direction.c
+
 void	look_north_south(double ra);
 void	look_west_east(double ra);
 void	west_east(double ra);
 void	north_south(double ra);
-void	init_colors(void);
-int		check_map(void);
-int		validate_string(char *input_str);
-int		render_hook(void);
-int		check_string(char *input_str);
-void	handle_exit(int keycode);
-int		check_images(void);
-void	rays(void);
-double	distance(int ax, int ay, int bx, int by);
-int		get_pixel(t_img img, int pixel);
-void	parser(char *file);
-int		check_format(const char *file);
-void	color_limits(int *red, int *green, int *blue);
-int		rgb_to_int(int red, int green, int blue);
-int		check_count(void);
-int		array_size(char **array);
-void	free_array(char **array);
-int		is_nbr(char *nbr);
-t_img	wall_image(void);
-void	wall_collision(void);
+
+// draw_utils.c
+
 void	draw_utils(void);
+void	wall_collision(void);
+t_img	wall_image(void);
+void	draw_floor_ceiling(int width, int height, t_pos pos);
+
+// draw.c
+
+void	rays(void);
+void	draw_all(void);
+
+// exit.c
+
+void	free_array(char **array);
+int		free_all(int exit_status);
+void	handle_exit(int keycode);
+
+// hook.c
+
+int		key_hook(int keycode);
+int		render_hook(void);
+
+// image.c
+
+void	put_pixel(t_img img, int x, int y, int color);
+int		get_pixel(t_img img, int pixel);
+t_img	new_image(int w, int h, t_win window);
+t_win	init_window(int w, int h, char *str);
+
+// inits.c
+
+void	init_rays(void);
+t_pos	init_player_pos(void);
+void	inits(char *file_path);
+
+// parser_utils.c
+
+int		validate_string(char *input_str);
+
+// parser_utils2.c
+
+int		array_size(char **array);
+int		check_string(char *input_str);
+int		check_count(void);
+int		check_format(const char *file);
+int		is_nbr(char *nbr);
+
+// parser.c
+
+void	parser(char *file);
+
+// utils.c
+t_var	*vars(void);
+int		check_images(void);
+double	distance(int ax, int ay, int bx, int by);
 
 #endif
